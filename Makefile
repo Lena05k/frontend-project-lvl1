c7c3ff23e1dff4c42d60059dc1a9bd5a172801db
Elena Krupoderova <lena05k@yandex.ru>
2021-12-16 19:15:21 +0300

    add .eslintrc.yml and nodejs.yml

--- Makefile
+++ Makefile
@@ -6,3 +6,7 @@ brain-games:
 
 publish:
 	npm publish --dry-run
+
+lint: 
+	npx eslint .
+
--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,4 @@
 import readlineSync from 'readline-sync';
 
-export const userName = readlineSync.question('May I have your name? ');
-
-
+const userName = readlineSync.question('May I have your name?');
+export default userName;
