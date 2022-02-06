6c7d570be4b201898bb1f49bc2049b6c82fabd3b
Elena Krupoderova <lena05k@yandex.ru>
2022-02-06 14:51:03 +0300

    Created a game 'prime'

---
+++ bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import prime from '../src/games/prime.js';
+
+prime();
---
+++ src/games/prime.js
@@ -0,0 +1,25 @@
+import engine from '../index.js';
+
+const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const findPrimeNumber = (number) => {
+  for (let i = 2; i < number; i += 1) {
+    if(number % i === 0) {
+      return false;
+    }
+  }
+  if (number < 2) {
+    return false;
+  }
+  return true;
+};
+
+const gameCheck = () => {
+  const number = Math.round(Math.random() * 10);
+  const question = `${number}`;
+  const correctAnswer = findPrimeNumber(number) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+const prime = () => engine(gameRules, gameCheck);
+
+export default prime;
