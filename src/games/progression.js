e88818107da76d807514d9f4abba1b8ce4390829
Elena Krupoderova <lena05k@yandex.ru>
2022-02-06 14:00:08 +0300

    I fixed the logic.

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,8 +3,8 @@ import engine from '../index.js';
 const gameRules = 'What number is missing in the progression?';
 
 const arithmeticProgression = (number, difference) => {
-  const array = [number];
-  for (let i = 0; i <= 9; i += 1) {
+  const array = [];
+  for (let i = 0; i <= 10; i += 1) {
     if (array.length === 10) {
       return array;
     } else {
@@ -17,13 +17,13 @@ const arithmeticProgression = (number, difference) => {
 };
 
 const gameCheck = () => {
-  const number = Math.round(Math.random() * 100);
+  const number = Math.round(Math.random() * 10);
   const difference = Math.round(Math.random() * 10);
 
   const progress = arithmeticProgression(number, difference);
+  const correctAnswer = `${progress[number]}`;
   progress[number] = '..';
-  const question = `${progress.join('..')}`;
-  const correctAnswer = progress[number];
+  const question = progress.join(' ');
   return [question, correctAnswer];
 };
 const progression = () => engine(gameRules, gameCheck);
