06ae8cd6b4958120a08d732d1f74802f2536b8a9
Elena Krupoderova <lena05k@yandex.ru>
2022-02-07 14:32:29 +0300

    Checking the code

--- src/games/calculator.js
+++ src/games/calculator.js
@@ -14,11 +14,11 @@ const equation = (num1, operator, num2) => {
 };
 
 const gameCheck = () => {
-  const num1 = Math.round(Math.random() * 100);
-  const num2 = Math.round(Math.random() * 100);
+  const number1 = Math.round(Math.random() * 100);
+  const number2 = Math.round(Math.random() * 100);
   const randomOperation = operations[Math.round(Math.random() * operations.length)];
-  const question = `${num1}${randomOperation}${num2}`;
-  const correctAnswer = `${equation(num1, randomOperation, num2)}`;
+  const question = `${number1}${randomOperation}${number2}`;
+  const correctAnswer = `${equation(number1, randomOperation, number2)}`;
   return [question, correctAnswer];
 };
 
