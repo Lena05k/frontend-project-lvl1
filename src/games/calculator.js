5c964234a575ccdd908ffac117e6cd9fae1c2119
Elena Krupoderova <lena05k@yandex.ru>
2022-02-13 14:49:20 +0300

    ...

--- src/games/calculator.js
+++ src/games/calculator.js
@@ -2,27 +2,24 @@ import engine from '../index.js';
 
 const gameRules = 'What is the result of the expression?';
 
-const operations = ['+', '-', '*'];
-
 const equation = (num1, operator, num2) => {
-  let result;
-  if (operator === '+') {
-    result = (num1 + num2);
-  } else if (operator === '-') {
-    result = (num1 - num2);
-  } else if (operator === '*') {
-    result = (num1 * num2);
-  } else if (operator === undefined) {
-    throw new Error('Unsupported operator');
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      return false;
   }
-  return result;
 };
 
 const gameCheck = () => {
+  const operations = ['+', '-', '*'];
+  const randomOperation = operations[Math.round(Math.random() * operations.length)];
   const number1 = Math.round(Math.random() * 10);
   const number2 = Math.round(Math.random() * 10);
-  const randomOperation = operations[Math.round(Math.random() * operations.length)];
-  console.log(randomOperation);
   const question = `${number1}${randomOperation}${number2}`;
   const correctAnswer = `${equation(number1, randomOperation, number2)}`;
   return [question, correctAnswer];
