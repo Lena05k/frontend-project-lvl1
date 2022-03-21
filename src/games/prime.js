0a362de82efe980f8ab1a6b7203498344fde97a6
Lena05k <lena05k@yandex.ru>
2022-03-21 16:20:49 +0300

    ...

--- src/games/prime.js
+++ src/games/prime.js
@@ -7,8 +7,10 @@ const isPrimeNumber = (number) => {
   if (number < 2) {
     return false;
   }
-  if (number % 2 === 0) {
-    return false;
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
   }
   return true;
 };
